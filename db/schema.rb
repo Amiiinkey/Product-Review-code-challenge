5637d2cf2561bb765a0f29cbc93b32698d5de670
Mohamed <aminito432@gmail.com>
2023-02-26 23:16:09 +0300

    added the seed.rb file and checked if the files are running as expected

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_26_042741) do
+ActiveRecord::Schema.define(version: 2023_02_26_193401) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.integer "star_rating"
     t.string "comment"
     t.integer "product_id"
     t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
   end
 
 end
