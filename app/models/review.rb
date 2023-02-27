254084e3ba8a557e1cc32e8a657e3f8853197743
Mohamed <aminito432@gmail.com>
2023-02-27 14:38:49 +0300

    updated the README file

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,7 @@ class Review < ActiveRecord::Base
     belongs_to :product
 
     def print_review
-        
+    #     puts "Review for #{product.name} by #{user.name}: #{self.star_rating}. #{self.comment}
+    # end
     end
 end
